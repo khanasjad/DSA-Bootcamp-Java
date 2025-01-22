cf7143f658ba6c6802968ac20821ae2ed14e3e6a
khanasjad <asjadkhan1994@yahoo.com>
2025-01-22 13:06:52 -0600

    New

---
+++ Study/src/Generic/Singleton.java
@@ -0,0 +1,34 @@
+package Generic;
+
+import jdk.internal.math.FormattedFloatingDecimal;
+
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
+
+import java.util.ArrayList;
+
+
+class Abc{
+
+    static Abc obj = new Abc();
+
+    private Abc(){
+
+    }
+
+    static Abc getInstance(){
+        return obj;
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("***********************Singleton******************");
+     Abc abc = Abc.getInstance();
+     Abc abc1 = Abc.getInstance();
+
+
+
+    }
+}
