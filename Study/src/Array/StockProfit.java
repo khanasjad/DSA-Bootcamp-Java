0052d580e35b6c06dcf5b9cdeeed54f606de9faa
khanasjad <asjadkhan1994@yahoo.com>
2025-01-22 12:28:36 -0600

    New Changes

---
+++ Study/src/Array/StockProfit.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.HashMap;
+
+public class StockProfit {
+
+    public static void main(String[] args){
+        int[] nums1 = new int[] {3,2,3};
+
+
+        majorityElement(nums1);
+    }
+
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+
+        for(int i = 0;i<nums.length ;i++){
+            if(hashMap.containsKey(nums[i])){
+                hashMap.put(nums[i],hashMap.get(nums[i])+1);
+                if(hashMap.get(nums[i]) > nums.length/2){
+                    System.out.println(nums[i]);
+                    return nums[i];
+                }}
+                else {
+                    hashMap.put(nums[i],1);
+                }
+
+
+
+
+
+        }
+        return 0;
+    }
+    }
