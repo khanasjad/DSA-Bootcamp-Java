37b33f157938722a687eec88326a1c1f907500db
khanasjad <asjadkhan1994@yahoo.com>
2025-01-29 14:22:06 -0600

    Update TrappingRainwater.java

--- Study/src/LeetCode/TwoPointers/TrappingRainwater.java
+++ Study/src/LeetCode/TwoPointers/TrappingRainwater.java
@@ -1,4 +1,41 @@
 package LeetCode.TwoPointers;
 
 public class TrappingRainwater {
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(height)); // Output: 6
+    }
+    public static int trap(int[] height){
+
+        int left = 0;
+        int right = height.length-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int totalWater= 0;
+
+        while (left<right){
+            if (height[left]<height[right]) {
+                if (leftMax <= height[left]) {
+                    leftMax = height[left];
+
+                } else {
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            }else {
+
+                if (rightMax < height[right]) {
+                    rightMax = height[right];
+
+                } else {
+                    totalWater += rightMax - height[right];
+                }
+                right--;
+            }
+
+
+        }
+
+       return totalWater;
+    }
 }
