5ef1ac41a8049855f19bd51e199d7951c0498e2f
khanasjad <asjadkhan1994@yahoo.com>
2025-02-01 16:36:34 -0600

    new changes

--- Study/src/LeetCode/Subsets/GenerateAllSubsets.java
+++ Study/src/LeetCode/Subsets/GenerateAllSubsets.java
@@ -1,7 +1,35 @@
 package LeetCode.Subsets;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateAllSubsets {
     public static void main(String[] args) {
-        
+        int[] nums = {1,2,3};
+        System.out.println("Subsets: " + subsets(nums));
+
+    }
+
+    private static List<List<Integer>>  subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void generateSubsets(int index, int[] nums, ArrayList<Integer> current, List<List<Integer>> result) {
+        if (index == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        // Exclude current element
+        generateSubsets(index + 1, nums, current, result);
+
+        // Include current element
+        current.add(nums[index]);
+        generateSubsets(index + 1, nums, current, result);
+
+        // Backtrack (remove last element)
+        current.remove(current.size() - 1);
     }
 }
--- Study/src/LeetCode/Subsets/GeneratePermutations.java
+++ Study/src/LeetCode/Subsets/GeneratePermutations.java
@@ -1,4 +1,42 @@
 package LeetCode.Subsets;
-
+import java.util.*;
 public class GeneratePermutations {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println("Permutations: " + permute(nums));
+        
+    }
+
+    private static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generatePermutations(nums, 0, result);
+        return result;
+    }
+
+    private static void generatePermutations(int[] nums, int index, List<List<Integer>> result) {
+       if (index==nums.length){
+           List<Integer> permutaions = new ArrayList<>();
+           for (int num:nums){
+               permutaions.add(num);
+           }
+
+           result.add(permutaions);
+           return;
+       }
+
+        for(int i= index;i<nums.length;i++){
+            swap(nums,i,index);
+            generatePermutations(nums,index+1,result);
+            swap(nums,i,index);
+        }
+
+
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
 }
